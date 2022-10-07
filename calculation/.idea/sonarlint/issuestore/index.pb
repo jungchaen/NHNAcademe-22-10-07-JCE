7d38ce9ae92dfc1835fc96ce3014c40b8cf156f1
jungchaen <yllowrose452@naver.com>
2022-10-07 17:29:57 +0900

    역할을 나눔.

---
+++ calculation/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+b
+2src/main/java/com/nhnacademy/calculation/Main.java,0\1\0114d6c10a0c0ae0dc5798aa2e30b19c2bdb3a66
--- calculation/src/main/java/com/nhnacademy/calculation/Main.java
+++ calculation/src/main/java/com/nhnacademy/calculation/Main.java
@@ -2,6 +2,6 @@ package com.nhnacademy.calculation;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("계산기 만들어주세요!! - 승아: 더하기 , 채은: 나머지, 동빈: 곱하기 , 승혜: 나누기 ");
     }
 }
